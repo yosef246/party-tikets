5ae176854c0e4fd4620f4dcad555f08102f0b9fe
Your Name <you@example.com>
2025-12-01 13:14:54 +0200

    update

--- backend/final_project/app.js
+++ backend/final_project/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import serverless from "serverless-http";
 
 import newUserRouter from "./routes/auth.js";
 import postRouter from "./routes/post.js";
@@ -54,10 +53,8 @@ app.use("/api/post", postRouter);
 app.use("/api/tag", tagRouter);
 app.use("/api/payment", paymentRouter);
 
-// app.listen(port, "0.0.0.0", () => {
-//   console.log(`Example run on port ${port}!`);
-// });
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example run on port ${port}!`);
+});
 
-// export default app;
-
-export const handler = serverless(app);
+export default app;
