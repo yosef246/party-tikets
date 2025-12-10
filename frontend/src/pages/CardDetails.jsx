a5a1a7f2c5423b1016a92d9bd5c73d8dbdcd96d6
Your Name <you@example.com>
2025-12-10 11:52:51 +0200

    first commit

--- frontend/src/pages/CardDetails.jsx
+++ frontend/src/pages/CardDetails.jsx
@@ -70,7 +70,7 @@ export default function CardDetails() {
     if (!userId) return;
     async function fetchStats() {
       try {
-        const res = await fetch(`/api/post/${userId}stats`);
+        const res = await fetch(`/api/post/${userId}/stats`);
 
         const data = await res.json();
 
