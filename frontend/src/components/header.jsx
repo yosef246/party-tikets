c934dd1c83750022fdbd69d2b0fec08a916f953e
Your Name <you@example.com>
2026-01-23 15:24:35 +0200

    first commit

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -61,7 +61,7 @@ function Header() {
       </Link>
       <div
         ref={hamburgerRef}
-        className={styles.hamburger}
+        className={`${styles.hamburger} ${isOpen ? styles.rotate : ""}`}
         onClick={() => setIsOpen(!isOpen)}
       >
         ☰
